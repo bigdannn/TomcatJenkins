b798e1a2a51ad33e65f0d206b0f54450fce9149c
Bodan <bogdan.yakovenko.301@gmail.com>
2022-05-25 17:41:40 +0300

    ff

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -6,5 +6,7 @@ public class Testing {
 
         System.out.println("again");
         System.out.println("again");
+        System.out.println("again");
+        System.out.println("again");
     }
 }
