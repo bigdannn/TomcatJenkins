b374e5130f7b6ab953269fd5f504e67b93da84fb
Bodan <bogdan.yakovenko.301@gmail.com>
2022-05-25 18:18:38 +0300

    commit

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -3,12 +3,5 @@ public class Testing {
         System.out.println("God. help me.");
         System.out.println("again");
 
-        System.out.println("One more time");
-
-        System.out.println("One more time");
-
-        System.out.println("One more time");
-
-        System.out.println("One more time");
     }
 }
