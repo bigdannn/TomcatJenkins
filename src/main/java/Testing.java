b3f8237995880c69bf882cb0779b9b23538a62d1
Bodan <bogdan.yakovenko.301@gmail.com>
2022-05-25 17:50:09 +0300

    rr

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -3,10 +3,10 @@ public class Testing {
         System.out.println("God. help me.");
         System.out.println("again");
 
+        System.out.println("One more time");
 
-        System.out.println("again");
-        System.out.println("again");
-        System.out.println("again");
-        System.out.println("again");
+        System.out.println("One more time");
+
+        System.out.println("One more time");
     }
 }
